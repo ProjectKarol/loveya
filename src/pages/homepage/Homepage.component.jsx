2896be5b91d23a82dd9a4852c88dcdfc67848a44
Karol Szczesny <karolszczesny@outlook.com>
2019-12-01 21:57:20 +0100

    add stylus components, cleanup naming convensions

--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Directory from '../../components/directory/Directory.component';
 
 import './homepage.styles.scss';
+import HomePageContainer from './Homepage.styles';
 
 const HomePage = () => (
-  <div className='homepage'>
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
